d276ac73d864b055a312d32c40608639c4033d57
T-Code07 <70549266+T-Code07@users.noreply.github.com>
2021-06-28 10:39:31 -0500

    Ground_checking_works
    Followed Youtube vid that I used for the player movement and used his ground check code.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,10 @@ public class PlayerController : MonoBehaviour
     [Header("Jumping/Physics:")]
     [SerializeField] float m_jumpHeight = 5f;
     [SerializeField] float m_gravity = -9.8f;
+    [SerializeField] Transform m_groundCheck;
+    [SerializeField] float m_groundDistance = 0.4f;
+    [SerializeField] LayerMask m_groundMask;
+    private bool m_isGrounded;
     
     Vector3 m_velocity;
     private void Start()
@@ -24,7 +28,18 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-
+        //Physics.CheckSphere projects a sphere from the given position with the radius of the given distance 
+        //that returns true if any colliders are in it.
+        //If a layer mask is provided, that means only check colliders in this layer. 
+        m_isGrounded = Physics.CheckSphere(m_groundCheck.position, m_groundDistance, m_groundMask);
+        if(m_isGrounded && m_velocity.y < 0) 
+        {
+            //Person found that because the sphere is a little bit lower than the actual body,
+            //it will register that it is grounded before the body is on the ground.
+            //Therefore, move the player to the ground if it thinks it is grounded. 
+            //Set to height of the character.  
+            m_velocity.y = -m_controller.height;
+        }
         //The difference may be the direction needed to move?
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -35,8 +50,9 @@ public class PlayerController : MonoBehaviour
 
         //v needed to reach h (height):
         //v = sqrt(-2hg) (Physics)
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
         {
+            print("Jumping");
             m_velocity.y = Mathf.Sqrt(m_jumpHeight * -2 * m_gravity);
         }
 
@@ -56,10 +72,7 @@ public class PlayerController : MonoBehaviour
         m_velocity.y += m_gravity * Time.deltaTime;
         m_controller.Move(m_velocity * Time.deltaTime);
 
-        if (m_controller.isGrounded) 
-        {
-            m_velocity = Vector3.zero;
-        }
+       
 
     }
 }
