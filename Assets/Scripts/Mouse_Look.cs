a91a378b9cc6bf401f0bbf4b9ba45cfcf3fcf07f
T-Code07 <70549266+T-Code07@users.noreply.github.com>
2021-06-28 07:07:08 -0500

    Fixed Mouse_Look lag
    Check the comments around the code. Used Time.smoothDeltaTime instead of Time.deltaTime.

--- Assets/Scripts/Mouse_Look.cs
+++ Assets/Scripts/Mouse_Look.cs
@@ -13,20 +13,23 @@ public class Mouse_Look : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-
-    void Update()
+    //Camera might have moved in update, so update after Update()
+    private void LateUpdate()
     {
         //Code from Unity's example code: https://www.youtube.com/watch?v=_QajrabyTJc
 
-        float Mouse_Input_X = Input.GetAxis("Mouse X") * Time.deltaTime * m_MouseSensitivity;
-        float Mouse_Input_Y = Input.GetAxis("Mouse Y") * Time.deltaTime * m_MouseSensitivity;
+        //changed Time.deltaTime to Time.smoothDeltaTime. It was super bumpy and looked as if it was stepping to different grid steps.
+        //However, smoothDeltaTime helped with that. Apparently, (according to https://forum.unity.com/threads/time-smoothdeltatime.10253/) it limits the flucuaction in Time.Deltatime. 
+        //Technically, it is slower than Time.Deltatime, but the "hitch" is less noticable. 
+        float Mouse_Input_X = Input.GetAxis("Mouse X") * m_MouseSensitivity * Time.smoothDeltaTime;
+        float Mouse_Input_Y = Input.GetAxis("Mouse Y") * m_MouseSensitivity * Time.smoothDeltaTime;
 
          m_xRotation -= Mouse_Input_Y;
         m_xRotation = Mathf.Clamp(m_xRotation, -90f, 90f);
 
 
         transform.localRotation = Quaternion.Euler(m_xRotation, 0, 0);
-        m_playerTransform.Rotate(Vector3.up * Mouse_Input_X);
-
+        
+          m_playerTransform.Rotate(Vector3.up * Mouse_Input_X);
     }
 }
