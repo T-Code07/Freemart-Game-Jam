649e16a2677bd7eccd08c27b762ba29ceefd81dd
T-Code07 <70549266+T-Code07@users.noreply.github.com>
2021-06-30 11:00:39 -0500

    Circle-Keeper-Script-and-Scan-Area-Boost
    1. Circle-Keeper script on the Camera View objects to make sure they stay circular
    
    2. The scan area is now slightly bigger than the scale of the actual circle.

---
+++ Assets/Circle_Keeper.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this script to make sure the Camera View of a camera is circular.
+/// </summary>
+[ExecuteInEditMode]
+public class Circle_Keeper : MonoBehaviour
+{
+    void Update()
+    {
+        float x = transform.localScale.z;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+}
