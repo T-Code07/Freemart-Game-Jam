479f72ad9ea06e29024a3154a9049ec93d8d14db
T-Code07 <70549266+T-Code07@users.noreply.github.com>
2021-07-07 23:05:05 -0500

    Location-Point-Manager-Checks-for-1-trip
    The LocationPointManager checks to see if the start location has been reached and the end location has been reached, then it ends its search.
    todo:
    need to make the player go to the stop location then back to the start location to end the game. Not just go to the stop location.

--- Assets/Scripts/Managers/LocationPointManager.cs
+++ Assets/Scripts/Managers/LocationPointManager.cs
@@ -7,6 +7,8 @@ public class LocationPointManager : MonoBehaviour
 {
     [SerializeField] LocationPoint m_startLocation;
     [SerializeField] LocationPoint m_endLocation;
+
+    private bool m_playerFinishedCourse = false;
     void Start()
     {
         if(m_startLocation.LocationType != LocationType.START || m_endLocation.LocationType != LocationType.END) 
@@ -15,8 +17,28 @@ public class LocationPointManager : MonoBehaviour
         }
     }
 
+    //todo: need to make the player go to the stop location then back to the start location to end the game. Not just go to the stop location.
     void Update()
     {
+        if (m_playerFinishedCourse) return;
         
+        if (m_startLocation.PlayerHasArrived && m_endLocation.PlayerHasArrived == false)
+        {
+            print("Game in progress...");
+        }
+        else if(m_startLocation.PlayerHasArrived && m_endLocation.PlayerHasArrived) 
+        {
+            print("Game ended.");
+            m_playerFinishedCourse = true;
+        }
+        else if(!m_startLocation.PlayerHasArrived && !m_endLocation.PlayerHasArrived) 
+        {
+            print("waiting for game to start...");
+        }
+        else 
+        {
+            Debug.LogError("Player arrived at end location before start location. Please review location point settings.");
+        }
+
     }
 }
