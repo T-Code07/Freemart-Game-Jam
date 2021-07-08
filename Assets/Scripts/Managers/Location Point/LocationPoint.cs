ac9f2149ee504082fd08641e9d8cccf3baf0c776
T-Code07 <70549266+T-Code07@users.noreply.github.com>
2021-07-07 22:42:28 -0500

    Organized-Files

--- Assets/Scripts/Managers/Location Point/LocationPoint.cs
+++ Assets/Scripts/Managers/Location Point/LocationPoint.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Freemart.Sensors
+namespace Freemart.Managers.LocationPoint
 {
     public enum LocationType 
     {
